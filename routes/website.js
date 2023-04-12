418f8c62cb77ff0579033a4ae36fed167689973a
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-04-12 03:18:30 +0300

    added all users (CRUD), also authorization based on user level and also admin

--- routes/website.js
+++ routes/website.js
@@ -1,7 +1,9 @@
 const router = require("express").Router()
 const websiteCtrl = require('../controllers/website')
+const auth = require('../middleware/auth')
+
 router.get('', websiteCtrl.website_show_get)
-router.post('', websiteCtrl.website_create_post)
+router.post('', auth.isAuth, websiteCtrl.website_create_post)
 router.get('/all', websiteCtrl.website_index_get)
 router.get('/domain', websiteCtrl.website_showByDomain_get)
 router.get('/user', websiteCtrl.website_showByUser_get)
