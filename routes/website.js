53a4d0f00b400a3deffeb0bf39a3537726716463
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-04-12 15:07:07 +0300

    website list for admin panel

--- routes/website.js
+++ routes/website.js
@@ -8,11 +8,11 @@ router.use(methodOverride('_method'));
 
 router.get('', websiteCtrl.website_show_get)
 router.post('', auth.isAuth, websiteCtrl.website_create_post)
-router.put('', websiteCtrl.website_edit_put)
-router.delete('', websiteCtrl.website_delete)
+router.put('', auth.isAuth, websiteCtrl.website_edit_put)
+router.post('/delete', auth.isAuth, websiteCtrl.website_delete)
 router.post('', auth.isAuth, websiteCtrl.website_create_post)
-router.get('/all', websiteCtrl.website_index_get)
-router.get('/domain', websiteCtrl.website_showByDomain_get)
-router.get('/user', websiteCtrl.website_showByUser_get)
+router.get('/all', auth.isAdmin, websiteCtrl.website_index_get)
+router.get('/domain', auth.isAuth, websiteCtrl.website_showByDomain_get)
+router.get('/user', auth.isAuth, websiteCtrl.website_showByUser_get)
 
 module.exports = router
